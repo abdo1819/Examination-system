d8c30e9155c180a08e0bf4b325d32e494cb4adee
hossam said <Hussam.Said42@gmail.com>
2022-01-16 23:09:07 +0200

    'added-SP-for-all-courses-and-topic-related-tables'

--- initiateDB/init.sql
+++ initiateDB/init.sql
@@ -204,7 +204,7 @@ CREATE TABLE [MCQ]
 
 
 
-use Examination
+------------------------------------------------------------------------
 
 alter procedure Insert_Student @f_name varchar(20), @l_name varchar(20), @address varchar(30), @email varchar(20), @password varchar(30), @dept_name varchar(20), @id_std int output
 with encryption
@@ -221,6 +221,7 @@ declare @id_dept int
 		select 'Duplicate Email'
 	end catch
 
+------------------------------------------------------------------------
 
 alter procedure Insert_Instructor @f_name varchar(20), @l_name varchar(20), @address varchar(30), @email varchar(20), @password varchar(30), @sal int, @degree varchar(50), @hire_date date = getdate, @dept_name varchar(20), @id_ins int output
 with encryption
@@ -237,6 +238,7 @@ declare @id_dept int
 		select 'Duplicate Email'
 	end catch
 
+------------------------------------------------------------------------
 
 alter procedure Insert_Department @dept_id int, @dept_name varchar(20), @mgr_name varchar(20), @id_mgr int output
 with encryption
@@ -255,6 +257,7 @@ end
 else 
 	select 'duplicate Department ID'
 
+------------------------------------------------------------------------
 
 create procedure Delete_Department @dept_name varchar(20)
 with encryption
@@ -271,6 +274,7 @@ end
 else 
 	select 'No department with name ' + @dept_name
 
+------------------------------------------------------------------------
 
 create procedure Update_Department_Manager @dept_name varchar(20), @mgr_name varchar(20)
 with encryption
@@ -289,5 +293,141 @@ end
 else 
 	select 'No department with name ' + @dept_name
 
+------------------------------------------------------------------------
 
 
+create procedure Insert_Course @crs_id int, @crs_name varchar(20)
+with encryption
+as
+if not exists (select crs_id from [Course] where crs_id = @crs_id)
+		insert into [course] values(@crs_id, @crs_name)
+else 
+	select 'Duplicate ID'
+
+------------------------------------------------------------------------
+
+alter procedure Delete_Course @crs_name varchar(20)
+with encryption
+as
+if exists (select crs_name from [Course] where crs_name = @crs_name)
+		delete from [Course] where crs_name = @crs_name
+else 
+	select 'There is no course named ' + @crs_name
+
+------------------------------------------------------------------------
+
+alter procedure Insert_Topic @top_id int, @top_name varchar(20), @crs_name varchar(20)
+with encryption
+as
+declare @id_crs int
+if not exists (select top_id from [Topic] where top_id = @top_id)
+	begin
+		select @id_crs = crs_id from [Course] where crs_name = @crs_name
+		insert into Topic values (@top_id, @top_name, @id_crs)
+	end 
+else 
+	select 'This Topic already exists'
+
+------------------------------------------------------------------------
+
+create procedure Delete_Topic @top_name varchar(20)
+with encryption
+as
+if exists (select top_name from [Topic] where top_name = @top_name)
+	begin
+		delete from Topic where top_name = @top_name 
+	end 
+else 
+	select 'There is no topic named ' + @top_name
+
+------------------------------------------------------------------------
+
+alter procedure Assign_Course_to_Instructor @crs_name varchar(20), @ins_id int
+with encryption
+as
+if not exists (select crs_name from [course] where crs_name = @crs_name)
+	begin
+		if not exists (select @ins_id from [User] where usr_id = @ins_id)
+			begin try
+				insert into [Ins_Course] (crs_id, ins_id)
+				values ((select crs_id from [Course] where crs_name = @crs_name), @ins_id)
+			end try
+			begin catch
+				select 'the Instructor is already assgined to this course'
+			end catch
+		else
+			select 'There is no Instructor named ' + @ins_id
+	end
+else 
+	select 'There is no Course named ' + @crs_name
+
+------------------------------------------------------------------------
+
+alter procedure End_Course_with_Instructor @crs_name varchar(20), @ins_id int
+with encryption
+as
+if not exists (select crs_name from [course] where crs_name = @crs_name)
+	begin
+		if not exists (select usr_id from [User] where usr_id = @ins_id)
+			begin
+				delete from [Ins_Course] where
+				(ins_id = @ins_id and 
+				crs_id = (select crs_id from [Course] where crs_name = @crs_name))
+			end
+		else
+			select 'There is no Instructor with this ID'
+	end
+else 
+	select 'There is no Course named ' + @crs_name
+
+--------------------------------------------------------------------------
+
+create procedure Student_Take_course_with_Instructor @std_id int, @crs_id int, @ins_id int
+as
+if not exists (select usr_id from [User] where usr_id = @ins_id)
+and not exists (select usr_id from [User] where usr_id = @std_id)
+	begin
+		if not exists (select crs_id from [Course] where crs_id = @crs_id)
+			begin
+				if exists (select crs_id, ins_id from [Ins_Course] 
+				where (crs_id = @crs_id and ins_id = @ins_id))
+				insert into Course_Attendance (crs_id, std_id, ins_id)
+				values(@crs_id, @std_id, @ins_id)
+				
+				else
+					select 'This course is not assgined to this instructor'
+			end 
+		else
+			select 'There is no course with this ID'
+	end
+
+else
+	select 'Please check the Instructor and Student ID'
+
+-----------------------------------------------------------------------
+
+create procedure End_Course_for_Student @crs_name varchar(20), @std_id int
+with encryption
+as
+if exists (select crs_name from [course] where crs_name = @crs_name)
+	begin
+		if exists (select usr_id from [User] where usr_id = @std_id)
+			begin
+				declare @id_course int
+				select @id_course = crs_id from [Course] where crs_name = @crs_name
+					if exists (select crs_id, std_id from [Course_Attendance] 
+								where (crs_id = @id_course and std_id = @std_id))
+						begin
+							delete from [Course_Attendance]	
+							where (crs_id = @id_course and std_id = @std_id)
+						end
+					else
+						select 'This student does not take this course'
+			end
+		else
+			select 'There is no student with this ID'
+	end
+else 
+	select 'There is no Course named ' + @crs_name
+
+-------------------------------------------------------------------------
