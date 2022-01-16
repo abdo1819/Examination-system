8d7abf8cd1fd8b34c5cc632ab41e234a46a0047e
abdo hashem <ar1813@fayoum.edu.eg>
2022-01-16 19:05:20 +0200

    create student and instructor

---
+++ initiateDB/CRUD.sql
@@ -0,0 +1,158 @@
+USE Examination;
+GO
+
+-- create PRIVATE schema if not exist in the database
+IF NOT EXISTS (SELECT *
+FROM sys.schemas
+WHERE name = 'PRIVATE')
+BEGIN
+    EXEC('CREATE SCHEMA [PRIVATE] ');
+END
+GO
+
+-- User [usr_id, user_type, f_name, l_name, address, email, password]
+CREATE OR ALTER PROCEDURE [PRIVATE].[Insert_User]
+    @user_type varChar(1),
+    @f_name varChar(50),
+    @l_name varChar(50),
+    @address varChar(150),
+    @email varChar(90),
+    @password varChar(255),
+    @usr_id INTEGER OUTPUT
+AS
+BEGIN
+    BEGIN TRY 
+    
+    
+
+    DECLARE @hashed_password varChar(255);
+    -- TODO define the seed globally
+    SELECT @hashed_password = HASHBYTES('SHA2_256', @password+'seed');
+    INSERT INTO [User]
+        (user_type, f_name, l_name, address, email, [password])
+    VALUES
+        (
+            @user_type,
+            @f_name,
+            @l_name,
+            @address,
+            @email,
+            @hashed_password
+    );
+    
+    SELECT @usr_id = scope_identity();
+    /* NOTE scope_identity() may give wrong result when queries run in parrallel
+    ref:[1]:https://blog.sqlauthority.com/2009/03/24/sql-server-2008-scope_identity-bug-with-multi-processor-parallel-plan-and-solution/
+    [2]:https://stackoverflow.com/questions/42648/sql-server-best-way-to-get-identity-of-inserted-row
+    */
+    END TRY
+    BEGIN CATCH
+    select ERROR_MESSAGE() 'Error Message'
+	, ERROR_NUMBER() 'Error Number'
+	, ERROR_LINE () 'Error Line Number'
+	, ERROR_SEVERITY () 'Error Severity Level'
+	, ERROR_PROCEDURE() 'Error Procedure'
+	, ERROR_STATE () 'Error State';
+    END CATCH
+END
+GO
+
+-- Student [std_id, dept_id]
+/*
+user has type 'S' capital S
+*/
+CREATE OR ALTER PROCEDURE [dbo].[Insert_Student]
+    @f_name varChar(50),
+    @l_name varChar(50),
+    @address varChar(150),
+    @email varChar(90),
+    @password varChar(255),
+    @dept_id INTEGER,
+    @stu_id INTEGER OUTPUT
+AS
+BEGIN
+    Exec [PRIVATE].[Insert_User] 'S', @f_name, @l_name, @address, @email, @password, @stu_id OUTPUT;
+    INSERT INTO [Student]
+    VALUES
+        (
+            @stu_id,
+            @dept_id
+    );
+END
+GO
+
+
+-- Instructor [ins_id, salary, degree, dept_id]
+/*
+user has type 'I' capital I
+*/
+CREATE OR ALTER PROCEDURE [dbo].[Insert_Instructor]
+    @f_name varChar(50),
+    @l_name varChar(50),
+    @address varChar(150),
+    @email varChar(90),
+    @password varChar(255),
+    @salary MONEY,
+    @degree varChar(50),
+    @dept_id INTEGER,
+    @ins_id INTEGER OUTPUT
+AS
+BEGIN
+    DECLARE @usr_id INTEGER;
+    Exec [PRIVATE].[Insert_User] 'I', @f_name, @l_name, @address, @email, @password, @usr_id OUTPUT;
+    INSERT INTO [Instructor](ins_id, salary, degree, dept_id)
+    VALUES
+        (
+            @usr_id,
+            @salary,
+            @degree,
+            @dept_id
+    );
+    SET @ins_id = @usr_id;
+END
+GO
+
+
+-- Department [dept_id, dept_name, mgr_id]
+-- TEMP 
+-- TODO remove this and replace it with the real procedure
+CREATE OR ALTER PROCEDURE [PRIVATE].[Insert_Department]
+    @dept_name varChar(100),
+    @mgr_id INTEGER,
+    @dept_id INTEGER OUTPUT
+AS
+BEGIN
+    INSERT INTO [Department]
+    VALUES
+        (
+            @dept_name,
+            @mgr_id
+    );
+    set @dept_id = scope_identity();
+    -- TODO handle case if mgr_id is not in the database
+END
+GO
+-- create a department with it's manager
+CREATE OR ALTER PROCEDURE [dbo].[Insert_Department_With_Manager]
+    @dept_name varChar(100),
+    @f_name varChar(50),
+    @l_name varChar(50),
+    @address varChar(150),
+    @email varChar(90),
+    @password varChar(255),
+    @salary MONEY,
+    @degree varChar(50),
+    @dept_id INTEGER OUTPUT,
+    @mgr_id INTEGER OUTPUT
+AS
+BEGIN
+    -- TODO use try catch for errors
+    ALTER TABLE Instructor NOCHECK CONSTRAINT Instructor_fk_1;
+    DECLARE @_no_dep INT = 0;
+    Exec [dbo].[Insert_Instructor] @f_name, @l_name, @address, @email, @password, @salary, @degree, @_no_dep, @mgr_id OUTPUT;
+    -- TODO replace this with the real procedure
+    Exec [PRIVATE].[Insert_Department] @dept_name, @mgr_id, @dept_id OUTPUT;
+    UPDATE Instructor SET dept_id = @dept_id WHERE ins_id = @mgr_id;
+    ALTER TABLE Instructor CHECK CONSTRAINT Instructor_fk_1;
+
+END
