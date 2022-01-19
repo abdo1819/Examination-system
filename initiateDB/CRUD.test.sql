de5efc1563d6bd29b99d9ab2185f59feebaf91f1
abdo hashem <ar1813@fayoum.edu.eg>
2022-01-19 16:40:27 +0200

    pretify testing suite

--- initiateDB/CRUD.test.sql
+++ initiateDB/CRUD.test.sql
@@ -2,22 +2,71 @@ use Examination;
 GO
 BEGIN TRANSACTION;
 
-    DECLARE @dept_id INTEGER, @mgr_id INTEGER;
-    
-    EXEC Insert_Department_With_Manager 'Computer Science', 'Mikasa', 'Ackermann', '123 Main St', 'Mikasa@mail.com', 'password', 10500, NULL, @dept_id OUTPUT,@mgr_id OUTPUT;
-    select @dept_id, @mgr_id;
-    IF (@dept_id IS NOT NULL and @mgr_id IS NOT NULL)
-        select 'Insert_Department_With_Manager passed' msg;
-    
-    declare @ins_id INTEGER;
-    EXEC Insert_Instructor 'Daz', 'Daz', '123 Main St', 'Daz@mail.com', 'password', 10500, 'Computer Science', @dept_id, @ins_id;
-    select @ins_id;
-    IF (@ins_id IS NOT NULL)
-        PRINT 'Insert_Instructor passed';
+/* -------------------------------------------------------------------------- */
+/*                        create student and instructor                       */
+/* -------------------------------------------------------------------------- */
 
-    DECLARE @student_id INTEGER;
-    EXEC Insert_Student 'Armin', 'Arlelt', '123 Main St', 'Armin@mail.com', 'password', @dept_id, @student_id;
-    select @student_id;
-    IF (@student_id IS NOT NULL)
-        PRINT 'Insert_Student passed';
+/* ------------- should insert new instructor and deparment ------------ */
+DECLARE @test_result TABLE (test_name VARCHAR(100),
+    test_result int);
+DECLARE @dept_id INTEGER, @mgr_id INTEGER;
+
+EXEC Insert_Department_With_Manager @dept_name = 'Computer Science',
+    @f_name =  'Mikasa',
+    @l_name =  'Ackermann',
+    @address =  '123 Main St',
+    @email =  'Mikasa@mail.com',
+    @password =  'password',
+    @salary =  10500,
+    @degree =  NULL,
+    @dept_id =  @dept_id OUTPUT,
+    @mgr_id = @mgr_id OUTPUT;
+
+-- select @dept_id, @mgr_id;
+IF (@dept_id IS NOT NULL and @mgr_id IS NOT NULL)
+        insert into @test_result
+VALUES('Insert_Department_With_Manager', 1);
+    ELSE
+        insert into @test_result
+VALUES('Insert_Department_With_Manager', 0);
+
+/* ------------ should insert new instructor if department exist ------------ */
+declare @ins_id INTEGER;
+EXEC Insert_Instructor @f_name = 'Daz',
+    @l_name =  'Daz',
+    @address =  '123 Main St',
+    @email =  'Daz@mail.com',
+    @password =  'password',
+    @salary =  10500,
+    @degree =  'Computer Science',
+    @dept_id =  @dept_id,
+    @ins_id =  @ins_id OUTPUT;
+IF (@ins_id IS NOT NULL)
+        insert into @test_result
+VALUES('Insert_Instructor', 1);
+    ELSE
+        insert into @test_result
+VALUES('Insert_Instructor', 0);
+
+
+/* ------------------------ should insert new student ----------------------- */
+DECLARE @student_id INTEGER;
+EXEC Insert_Student @f_name ='Armin',
+    @l_name = 'Arlelt',
+    @address = '123 Main St',
+    @email = 'Armin@mail.com',
+    @password = 'password',
+    @dept_id = @dept_id,
+    @stu_id = @student_id OUTPUT;
+select @student_id;
+IF (@student_id IS NOT NULL)
+    insert into @test_result
+VALUES('Insert_Student', 1);
+ELSE
+        insert into @test_result
+VALUES('Insert_Student', 0);
+
+
+SELECT *
+from @test_result;
 COMMIT
