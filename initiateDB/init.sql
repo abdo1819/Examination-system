f0ff37b098c198cd9a6b07b78a45a2e58b1f63aa
hossam said <Hussam.Said42@gmail.com>
2022-01-19 22:42:42 +0200

    reviewed the procedures and edit tables

--- initiateDB/init.sql
+++ initiateDB/init.sql
@@ -87,8 +87,8 @@ CREATE TABLE [Instructor]
 
 CREATE TABLE [Course]
 (
-    crs_id INTEGER PRIMARY KEY ,
-    crs_name varChar(100) NOT NULL,
+    crs_id INTEGER PRIMARY KEY IDENTITY(1000, 100),
+    crs_name varChar(100) UNIQUE NOT NULL,
 );
 
 -- Course_Attendance [crs_id, std_id, ins_id, grade]
@@ -99,9 +99,9 @@ CREATE TABLE [Course_Attendance]
     std_id INTEGER ,
     ins_id INTEGER ,
     grade INTEGER , -- The grade of the last exam taken by student in this course -- Derived
-    FOREIGN KEY (crs_id) REFERENCES Course(crs_id),
-    FOREIGN KEY (std_id) REFERENCES Student(std_id),
-    FOREIGN KEY (ins_id) REFERENCES Instructor(ins_id),
+    FOREIGN KEY (crs_id) REFERENCES Course(crs_id) on delete cascade ,
+    FOREIGN KEY (std_id) REFERENCES Student(std_id) on delete cascade,
+    FOREIGN KEY (ins_id) REFERENCES Instructor(ins_id) ,
 	PRIMARY KEY (crs_id,std_id,ins_id)
 );
 -- Ins_Course [crs_id, ins_id, evaluation, dept_id]
@@ -111,8 +111,8 @@ CREATE TABLE [Ins_Course]
     crs_id INTEGER ,
     ins_id INTEGER ,
     evaluation INTEGER ,
-    FOREIGN KEY (crs_id) REFERENCES Course(crs_id) ,
-    FOREIGN KEY (ins_id) REFERENCES Instructor(ins_id) ,
+    FOREIGN KEY (crs_id) REFERENCES Course(crs_id) on delete cascade,
+    FOREIGN KEY (ins_id) REFERENCES Instructor(ins_id) on delete cascade,
 	CONSTRAINT c_CA_PK PRIMARY KEY (crs_id,ins_id),
 	CONSTRAINT c_IC_eval CHECK (evaluation BETWEEN 0 AND 10)
 );
@@ -121,8 +121,8 @@ CREATE TABLE [Ins_Course]
 
 CREATE TABLE [Department]
 (
-    dept_id INTEGER PRIMARY KEY ,
-    dept_name varChar(100) NOT NULL,
+    dept_id INTEGER PRIMARY KEY IDENTITY(100, 100),
+    dept_name varChar(100) UNIQUE NOT NULL,
     mgr_id INTEGER NOT NULL ,
     FOREIGN KEY (mgr_id) REFERENCES Instructor(ins_id) , -- Check Constraints later
 );
@@ -134,8 +134,8 @@ ALTER TABLE [Instructor] ADD CONSTRAINT [Instructor_fk_1] FOREIGN KEY (dept_id) 
 
 CREATE TABLE [Topic]
 (
-    top_id INTEGER PRIMARY KEY ,
-    top_name varChar(100) NOT NULL,
+    top_id INTEGER PRIMARY KEY IDENTITY(10000, 1000),
+    top_name varChar(100) UNIQUE NOT NULL,
     crs_id INTEGER NOT NULL,
     FOREIGN KEY (crs_id) REFERENCES Course(crs_id) ON DELETE CASCADE,
 );
@@ -144,7 +144,7 @@ CREATE TABLE [Topic]
 
 CREATE TABLE [Exam]
 (
-    ex_id INTEGER PRIMARY KEY ,
+    ex_id INTEGER PRIMARY KEY IDENTITY(10, 10),
     date Date NOT NULL DEFAULT GETDATE(),
     crs_id INTEGER NOT NULL,
     FOREIGN KEY (crs_id) REFERENCES Course(crs_id) , -- Check constraints later
@@ -177,7 +177,7 @@ CREATE TABLE [Exam_Answer]
 -- Question [q_id, q_type, q_text, corr_answer, crs_id]
 CREATE TABLE [Question]
 (
-    q_id INTEGER PRIMARY KEY ,
+    q_id INTEGER PRIMARY KEY IDENTITY(500, 50),
     q_type varChar(3) NOT NULL,
     q_text varChar(300) NOT NULL,
     corr_answer varChar(1) NOT NULL,
@@ -203,232 +203,3 @@ CREATE TABLE [MCQ]
 );
 
 
-
-------------------------------------------------------------------------
-
-create or alter procedure Insert_Student @f_name varchar(20), @l_name varchar(20), @address varchar(30), @email varchar(20), @password varchar(30), @dept_name varchar(20), @id_std int output
-with encryption
-as
-declare @id_dept int
-	begin try
-		insert into [User] values ('s', @f_name, @l_name, @address, @email, @password)
-		select @id_std = usr_id from [User] where email = @email
-		select @id_dept = dept_id from [Department] where dept_name = @dept_name
-		insert into [Student] values (@id_std, @id_dept)
-		return @id_std
-	end try
-	begin catch
-		select 'Duplicate Email'
-	end catch
-
-------------------------------------------------------------------------
-
-create or alter procedure Insert_Instructor @f_name varchar(20), @l_name varchar(20), @address varchar(30), @email varchar(20), @password varchar(30), @sal int, @degree varchar(50), @hire_date date = getdate, @dept_name varchar(20), @id_ins int output
-with encryption
-as
-declare @id_dept int
-	begin try
-		insert into [User] values ('i', @f_name, @l_name, @address, @email, @password)
-		select @id_ins = usr_id from [User] where email = @email
-		select @id_dept = dept_id from [Department] where dept_name = @dept_name
-		insert into [Instructor] values (@id_ins, @sal, @degree, @id_dept, @hire_date)
-		return @id_ins
-	end try
-	begin catch
-		select 'Duplicate Email'
-	end catch
-
-------------------------------------------------------------------------
-
-create or alter procedure Insert_Department @dept_id int, @dept_name varchar(20), @id_mgr int output
-with encryption
-as
-if not exists (select dept_id from [Department] where dept_id = @dept_id)
-begin
-		if not exists (select usr_id from [User] where usr_id = @id_mgr)
-		begin
-			insert into [Department] values (@dept_id, @dept_name, @id_mgr)
-			return @id_mgr
-		end
-		else
-			select 'no instructor with ID ' + @id_mgr' found'
-end
-else 
-	select 'duplicate Department ID'
-
-------------------------------------------------------------------------
-
-create or alter procedure Delete_Department @dept_name varchar(20), @success int output
-with encryption
-as
-if exists (select dept_name from [Department] where dept_name = @dept_name)
-begin
-	delete from [Department] where dept_name = @dept_name
-	set @success = 1
-	return @success -- deleted successfully 
-end
-else 
-	select 'No department with name ' + @dept_name
-
-------------------------------------------------------------------------
-
-create or alter procedure Update_Department_Manager @dept_name varchar(20), @mgr_id varchar(20)
-with encryption
-as
-if exists (select dept_name from [Department] where dept_name = @dept_name)
-begin
-	begin try
-		update [Department] set mgr_id = @mgr_id where dept_name = @dept_name
-	end try
-	begin catch
-		select 'Error: There is no an instructor with ID ' + @mgr_id
-	end catch
-end
-else 
-	select 'No department with name ' + @dept_name
-
-------------------------------------------------------------------------
-
-
-create or alter procedure Insert_Course @crs_id int, @crs_name varchar(20)
-with encryption
-as
-if not exists (select crs_id from [Course] where crs_id = @crs_id)
-		insert into [course] values(@crs_id, @crs_name)
-else 
-	select 'Duplicate ID'
-
-------------------------------------------------------------------------
-
-create or alter procedure Delete_Course @crs_name varchar(20)
-with encryption
-as
-if exists (select crs_name from [Course] where crs_name = @crs_name)
-	begin
-		declare @id_course int
-		select @id_course = crs_id from [Course] where crs_name = @crs_name
-
-		delete from [Course_Attendance] where crs_id = @id_course
-		delete from Ins_Course] where crs_id = @id_course
-		delete from [Course] where crs_name = @crs_name
-	end
-else 
-	select 'There is no course named ' + @crs_name
-
-------------------------------------------------------------------------
-
-create or alter procedure Insert_Topic @top_id int, @top_name varchar(20), @crs_name varchar(20)
-with encryption
-as
-declare @id_crs int
-if not exists (select top_id from [Topic] where top_id = @top_id)
-	begin
-		select @id_crs = crs_id from [Course] where crs_name = @crs_name
-		insert into Topic values (@top_id, @top_name, @id_crs)
-	end 
-else 
-	select 'This Topic already exists'
-
-------------------------------------------------------------------------
-
-create or alter procedure Delete_Topic @top_name varchar(20)
-with encryption
-as
-if exists (select top_name from [Topic] where top_name = @top_name)
-	begin
-		delete from Topic where top_name = @top_name 
-	end 
-else 
-	select 'There is no topic named ' + @top_name
-
-------------------------------------------------------------------------
-
-create or alter procedure Assign_Course_to_Instructor @crs_name varchar(20), @ins_id int
-with encryption
-as
-if not exists (select crs_name from [course] where crs_name = @crs_name)
-	begin
-		if not exists (select @ins_id from [User] where usr_id = @ins_id)
-			begin try
-				insert into [Ins_Course] (crs_id, ins_id)
-				values ((select crs_id from [Course] where crs_name = @crs_name), @ins_id)
-			end try
-			begin catch
-				select 'the Instructor is already assgined to this course'
-			end catch
-		else
-			select 'There is no Instructor named ' + @ins_id
-	end
-else 
-	select 'There is no Course named ' + @crs_name
-
-------------------------------------------------------------------------
-
-create or alter procedure End_Course_with_Instructor @crs_name varchar(20), @ins_id int
-with encryption
-as
-if not exists (select crs_name from [course] where crs_name = @crs_name)
-	begin
-		if not exists (select usr_id from [User] where usr_id = @ins_id)
-			begin
-				delete from [Ins_Course] where
-				(ins_id = @ins_id and 
-				crs_id = (select crs_id from [Course] where crs_name = @crs_name))
-			end
-		else
-			select 'There is no Instructor with this ID'
-	end
-else 
-	select 'There is no Course named ' + @crs_name
-
---------------------------------------------------------------------------
-
-create or alter procedure Student_Take_course_with_Instructor @std_id int, @crs_id int, @ins_id int
-as
-if not exists (select usr_id from [User] where usr_id = @ins_id)
-and not exists (select usr_id from [User] where usr_id = @std_id)
-	begin
-		if not exists (select crs_id from [Course] where crs_id = @crs_id)
-			begin
-				if exists (select crs_id, ins_id from [Ins_Course] 
-				where (crs_id = @crs_id and ins_id = @ins_id))
-				insert into Course_Attendance (crs_id, std_id, ins_id)
-				values(@crs_id, @std_id, @ins_id)
-				
-				else
-					select 'This course is not assgined to this instructor'
-			end 
-		else
-			select 'There is no course with this ID'
-	end
-
-else
-	select 'Please check the Instructor and Student ID'
-
------------------------------------------------------------------------
-
-create or alter procedure End_Course_for_Student @crs_name varchar(20), @std_id int
-with encryption
-as
-if exists (select crs_name from [course] where crs_name = @crs_name)
-	begin
-		if exists (select usr_id from [User] where usr_id = @std_id)
-			begin
-				declare @id_course int
-				select @id_course = crs_id from [Course] where crs_name = @crs_name
-					if exists (select crs_id, std_id from [Course_Attendance] 
-								where (crs_id = @id_course and std_id = @std_id))
-						begin
-							delete from [Course_Attendance]	
-							where (crs_id = @id_course and std_id = @std_id)
-						end
-					else
-						select 'This student does not take this course'
-			end
-		else
-			select 'There is no student with this ID'
-	end
-else 
-	select 'There is no Course named ' + @crs_name
-
--------------------------------------------------------------------------
