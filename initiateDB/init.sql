8d206efec26921aadccd8044bbbe8ee38c4851a6
abdo hashem <ar1813@fayoum.edu.eg>
2022-01-19 20:50:44 +0200

    addd amdfsadf

--- initiateDB/init.sql
+++ initiateDB/init.sql
@@ -215,9 +215,9 @@ declare @id_dept int
 		select @id_std = usr_id from [User] where email = @email
 		select @id_dept = dept_id from [Department] where dept_name = @dept_name
 		insert into [Student] values (@id_std, @id_dept)
-		return @id_std
+		return @id_s
 	end try
-	begin catch
+	begin catchdsad
 		select 'Duplicate Email'
 	end catch
 
@@ -237,3 +237,25 @@ declare @id_dept int
 		select 'Duplicate Email'
 	end catch
 
+-- Create a new stored procedure called 'StoredProcedureName' in schema 'SchemaName'
+-- Drop the stored procedure if it already exists
+IF EXISTS (
+SELECT *
+	FROM INFORMATION_SCHEMA.ROUTINES
+WHERE SPECIFIC_SCHEMA = N'SchemaName'
+	AND SPECIFIC_NAME = N'StoredProcedureName'
+)
+DROP PROCEDURE SchemaName.StoredProcedureName
+GO
+-- Create the stored procedure in the specified schema
+CREATE PROCEDURE SchemaName.StoredProcedureName
+	@param1 /*parameter name*/ int /*datatype_for_param1*/ = 0, /*default_value_for_param1*/
+	@param2 /*parameter name*/ int /*datatype_for_param1*/ = 0 /*default_value_for_param2*/
+-- add more stored procedure parameters here
+AS
+	-- body of the stored procedure
+	SELECT @param1, @param2
+GO
+-- example to execute the stored procedure we just created
+EXECUTE SchemaName.StoredProcafdsfaedureName 1 /*value_for_param1*/, 2 /*value_for_param2*/
+GO
