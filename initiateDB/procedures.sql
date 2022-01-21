cb24e86552ea9e825206ba293abab727acb57095
Husam_42 <Hussam.said42@gmail.com>
2022-01-21 18:43:25 +0200

    report that takes course ID and returns its topics

--- initiateDB/procedures.sql
+++ initiateDB/procedures.sql
@@ -613,3 +613,21 @@ else
 
 GO
 
+-- Report that takes course ID and returns its topics
+GO
+
+create or alter procedure Topics_of_Course @crs_name varchar(20)
+as
+if exists(select crs_name from Course where crs_name = @crs_name)
+begin
+	select t.top_name
+	from Course c
+	inner join Topic t
+	on c.crs_id = t.crs_id
+	where c.crs_name = @crs_name
+end
+else
+	select 'There is no course named ' + @crs_name
+
+
+GO
