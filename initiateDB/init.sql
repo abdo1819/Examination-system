dd6a8b502f3c25bc43a71781c13530a85f702287
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-01-22 20:33:48 +0200

    formatting

--- initiateDB/init.sql
+++ initiateDB/init.sql
@@ -107,9 +107,14 @@ CREATE OR ALTER FUNCTION getStudentGrade(@crs_id INT, @std_id INT)
 RETURNS INT
 AS
 BEGIN
-DECLARE @result INT
-SELECT @result = (SELECT SUM(EA.std_mark) FROM Exam_Answer EA, Exam E WHERE EA.ex_id = E.ex_id AND E.crs_id = @crs_id AND EA.std_id = @std_id )
-	RETURN @result
+	DECLARE @result INT
+	SELECT @result = 
+	(
+		SELECT SUM(EA.std_mark) 
+		FROM Exam_Answer EA, Exam E 
+		WHERE EA.ex_id = E.ex_id AND E.crs_id = @crs_id AND EA.std_id = @std_id 
+	)
+		RETURN @result
 END
 go
 CREATE TABLE [Course_Attendance]
