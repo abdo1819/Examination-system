b2a7d5ac4dc862f7384a359bce75e948860913d0
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-02-17 16:18:33 +0200

    fixed connection string, added version of app with no crystal reports

---
+++ Desktop App/No Crystal Report App/FrmHome/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmHome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void userBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        ExaminationDataSet.getAllStudentsDataTable dt = new ExaminationDataSet.getAllStudentsDataTable();
+        ExaminationDataSet.Insert_StudentDataTable dtIns = new ExaminationDataSet.Insert_StudentDataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //getAllStudentsTableAdapter1.Fill(dt);
+            //foreach (var row in dt)
+            //{
+            //    Trace.WriteLine(row.l_name);
+            //}
+            int? stuId = 0;
+            insert_StudentTableAdapter1.Fill(dtIns, "das", "das", "address", "adsf@fssa", "dasda", 100, ref stuId);
+            dtIns.AcceptChanges();
+
+            Trace.WriteLine(stuId);
+            //foreach (var row in dtIns)
+            //{
+            //    Trace.WriteLine(row.);
+            //}
+        }
+    }
+}
