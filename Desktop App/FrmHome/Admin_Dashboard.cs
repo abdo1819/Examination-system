a372eb99122e1c0e5b48cfaf0fa2a2df028c2b9b
abdo hashem <ar1813@fayoum.edu.eg>
2022-02-20 21:22:56 +0200

    open reports with button

--- Desktop App/FrmHome/Admin_Dashboard.cs
+++ Desktop App/FrmHome/Admin_Dashboard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ReportsForm;
 
 namespace FrmHome
 {
@@ -57,8 +58,8 @@ namespace FrmHome
 
         private void btnReport_Click(object sender, EventArgs e)
         {
-           // Form1 frmReports = new Form1();
-            // frmReports.ShowDialog();
+            reportsForm frmReports = new reportsForm();
+            frmReports.ShowDialog();
             // There is an issue with opening this form
         }
 
