d34403d0ffa2a9bef411cb5baed500ff6933fc87
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-02-21 01:42:31 +0200

    Instructor windows progress

--- Desktop App/FrmCrystal/Reports/ViewExamWithAnswers.cs
+++ Desktop App/FrmCrystal/Reports/ViewExamWithAnswers.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FrmHome.Reports {
+namespace ReportsForm.Reports {
     using System;
     using System.ComponentModel;
     using CrystalDecisions.Shared;
@@ -41,7 +41,7 @@ namespace FrmHome.Reports {
         
         public override string FullResourceName {
             get {
-                return "FrmHome.Reports.ViewExamWithAnswers.rpt";
+                return "ReportsForm.Reports.ViewExamWithAnswers.rpt";
             }
             set {
                 // Do nothing
--- Desktop App/FrmHome/Admin_ManageCourses.cs
+++ Desktop App/FrmHome/Admin_ManageCourses.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,77 @@ namespace FrmHome
 {
     public partial class Admin_ManageCourses : Form
     {
-        public Admin_ManageCourses()
+        Login frmLogin;
+        Admin_Dashboard frmAdmin;
+        Entities.Course selectedCourse;
+        List<Entities.Topic> topics;
+        public Admin_ManageCourses(Login _frmLogin, Admin_Dashboard _frmAdmin)
         {
+            frmLogin = _frmLogin;
+            frmAdmin = _frmAdmin;
             InitializeComponent();
+        }
+        public void ChangeTopic()
+        {
+            var MCQs = frmLogin.Ctx.Question
+                .Where(q => q.q_type == "MCQ" && q.top_id == topics[comboTopics.SelectedIndex].top_id).ToList().Count;
+            var TFQs = frmLogin.Ctx.Question
+                .Where(q => q.q_type == "TF" && q.top_id == topics[comboTopics.SelectedIndex].top_id).ToList().Count;
+            lblTName.Text = topics[comboTopics.SelectedIndex].top_name;
+            lblMCQs.Text = MCQs.ToString();
+            lblTFQs.Text = TFQs.ToString();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            frmLogin.Close();
+            if (frmLogin.IsDisposed == false)
+                e.Cancel = true;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            lblStdID.Text = frmAdmin.UsrID;
+            lblName.Text = frmAdmin.EmpName;
+            lblDept.Text = frmAdmin.Dept;
+            lblEmail.Text = frmAdmin.Email;
+            lblAddress.Text = frmAdmin.Address;
+
+            var courses = frmLogin.Ctx.Course.ToList();
+            comboCourses.DataSource = courses;
+            comboCourses.DisplayMember = "crs_name";
+            base.OnLoad(e);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            frmAdmin.Show();
+            this.Close();
+        }
+
+        private void comboCourses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedCourse = (Entities.Course)comboCourses.SelectedItem;
+            topics = frmLogin.Ctx.Topic.Where(t => t.crs_id == selectedCourse.crs_id).ToList();
+            comboTopics.DataSource = topics;
+            comboTopics.DisplayMember = "top_id";
+            ChangeTopic();
+        }
+
+        private void comboTopics_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblTName.Text = topics[comboTopics.SelectedIndex].top_name;
+            ChangeTopic();
+        }
+
+        private async void btnViewQuestions_Click(object sender, EventArgs e)
+        {
+            var result = await frmLogin.Procedures.viewTopicMCQAsync(topics[comboTopics.SelectedIndex].top_name, new OutputParameter<int>());
+            dataGridQuestions.DataSource = result;
+        }
+
+        private async void btnViewTFQ_Click(object sender, EventArgs e)
+        {
+            var result = await frmLogin.Procedures.viewTopicTFQAsync(topics[comboTopics.SelectedIndex].top_name, new OutputParameter<int>());
+            dataGridQuestions.DataSource = result;
         }
     }
 }
